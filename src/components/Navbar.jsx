e3175859d8ad22802083f21da4bfd1c8ddd2b9b1
aguilanbon <aguilanbon@gmail.com>
2022-06-04 16:47:28 +0800

    added blog cards & nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <di className="navbar-container">
+        <div className="nav">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Log In</a></li>
+            </ul>
+        </div>
+    </di>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogCard from '../components/BlogCard'
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <h1>Welcome</h1>
+        <BlogCard />
+    </div>
+  )
+}
+
+export default Home
