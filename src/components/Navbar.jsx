6f866bdbde18bb99c4df74321795f3bd61c47922
aguilanbon <aguilanbon@gmail.com>
2022-06-06 10:18:42 +0800

    disable mobile nav on larger devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import MobileNav from './MobileNav'
 
 function Navbar({ setFormState }) {
 
+  const navRef = useRef(null)
   const [showMobileNav, setShowMobileNav] = useState(false)
 
+
+  useEffect(() => {
+    const closeNavOnScreenSiz = () => {
+      if (navRef.current.offsetWidth > 480) {
+        setShowMobileNav(false)
+      }
+    }
+
+    closeNavOnScreenSiz()
+  })
+
+
   return (
-    <div className="navbar-container">
+    <div className="navbar-container" ref={navRef}>
       <div className="nav">
         {!showMobileNav &&
           <ul>
-            <i onClick={() => setShowMobileNav(true)} id='hamburger-btn' style={{ color: 'white' }} class="fas fa-solid fa-bars"></i>
+            <i onClick={() => setShowMobileNav(true)} id='hamburger-btn' style={{ color: 'white' }} className="fas fa-solid fa-bars"></i>
             <li><Link to='/' >Home</Link></li>
             <li><Link to='/user/login' onClick={() => setFormState('login')}>Log in</Link></li>
           </ul>
